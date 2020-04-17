fa164443b7c4b7e03ceee5b5ae4d3d04dca375fd
augusto <augustoadsis@gmail.com>
2020-04-17 14:05:29 -0300

    path adjustments

--- course/src/main/java/com/microservices/CourseApplication.java
+++ course/src/main/java/com/microservices/CourseApplication.java
@@ -2,8 +2,10 @@ package com.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(scanBasePackages = {"com.microservices.core", "com.microservices.course"})
+@EnableEurekaClient
 public class CourseApplication {
 
     public static void main(String[] args) {
